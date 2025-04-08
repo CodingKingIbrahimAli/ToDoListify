829dea5b699205965f1d98fd3967f6976fc84032
CodingKingIbrahimAli <worldaction396@gmail.com>
2025-04-08 11:23:45 +0300

    do register page

--- frontend/js/start.js
+++ frontend/js/start.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const axiosInstance = axios.create({
         baseURL: baseURL,
         withCredentials: true,
-      });
+    });
     let accessToken = localStorage.getItem("accessToken");
 
     async function checkLoged() {
@@ -39,3 +39,44 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 2500)
     }
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    // late movement   --   just one second
+    setTimeout(() => {
+        animateElement("burble-left", 100, 1); // left burble movement 
+    }, 1000);
+
+    setTimeout(() => {
+        animateElement("burble-right", -100, 1); // right burble movement 
+    }, 1500);
+
+    setTimeout(() => {
+        fadeInElement("text-container"); // text apperance 
+    }, 2000);
+
+    setTimeout(() => {
+        slideInElement("underline"); // underline move 
+    }, 2500);
+});
+
+// function of burbles movement
+function animateElement(id, translateX, duration) {
+    let element = document.getElementById(id);
+    element.style.transition = `opacity ${duration}s ease-out, transform ${duration}s ease-out`;
+    element.style.opacity="1";
+    element.style.transform = `translateX(${translateX}px)`;
+}
+
+// function of text
+function fadeInElement(id) {
+    let element = document.getElementById(id);
+    element.style.transition = "opacity 1s ease-out";
+    element.style.opacity = "1";
+}
+
+// function of underline
+function slideInElement(id) {
+    let element = document.getElementById(id);
+    element.style.transition = "transform 1s ease-out";
+    element.style,transform = "translateX(0)";
+}
